6bc1130edf61b09f23f56f82c996973f4e24ebdf
Rishab Raj <rr0948555@gmail.com>
2026-04-04 19:38:58 +0530

    Register

--- frontend-react/src/components/Register.jsx
+++ frontend-react/src/components/Register.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Register = () => {
+  const[username, setUsername] = useState("");
+  const[email, setEmail] = useState("");
+  const[password, setPassword] = useState("");
+
+  const handleRegistration = (e) => {
+      e.preventDefault();
+      console.log('test');
+  }
+
   return (
-    <div className='text-light container'>Register</div>
+     <>
+       <div className='container'>
+            <div className='row justify-content-center'>
+                   <div className='col-md-6 bg-light-dark p-5 rounded'>
+                       <h3 className='text-light text-center mb-4'>Create an Account</h3>
+                       <form onSubmit={handleRegistration}>
+                          <input type="text" className='form-control mb-3' placeholder=' Username ' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                          <input type="email" className='form-control mb-3' placeholder=' Email address' value={email}  onChange={(e) => setEmail(e.target.value)}/>
+                          <input type="password" className='form-control mb-5' placeholder='Set password' value={password}  onChange={(e) => setPassword(e.target.value)}/>
+                          <button type='submit' className='btn btn-success d-block mx-auto'>Register</button>
+                       </form>
+                   </div>
+            </div>
+       </div>
+     </>
   )
 }
 
