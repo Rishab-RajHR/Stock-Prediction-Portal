c410825515d89056d9fb75d928b1d9b5b95498aa
Rishab Raj <rr0948555@gmail.com>
2026-04-09 19:58:17 +0530

    Dasboard

--- frontend-react/src/components/Main.jsx
+++ frontend-react/src/components/Main.jsx
@@ -9,7 +9,7 @@ const Main = () => {
            <div className='p-5 text-center bg-light-dark rounded'>
                <h1 className='text-light'>Stock Prediction Portal</h1>
                <p className='text-light lead'>A Stock Prediction Portal allows users to monitor and analyze stock market data while predicting future price trends using machine learning. Built with React and Django, it fetches real-time or historical stock data, displays it through interactive charts, and applies predictive algorithms to forecast prices. Users can search for specific stocks, view past performance, and get estimated future values. </p>
-                <Button text="Login" class="btn-outline-info" />
+                <Button text="Explore Now" class="btn-info" url="/dashboard" />
            </div>
       </div>
      </>
